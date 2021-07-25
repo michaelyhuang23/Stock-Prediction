9b9290ff7f3a930dbd6fb84090232cf123fb1636
Michael Huang <michaelyhuang23@gmail.com>
2021-07-25 00:54:41 -0400

    model training log

--- rnn_baseline_trainer.py
+++ rnn_baseline_trainer.py
@@ -1,8 +1,9 @@
 from RNNbaseline import RecurrentAnalyzer
 from torch import nn
 import torch
-from torch.optim import Adam
+from torch.optim import Adam, SGD
 from torch.utils.tensorboard import SummaryWriter
+# serve command: tensorboard --logdir=runs  
 from StockDatabase import StockDatabase
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -14,26 +15,28 @@ database.read_data()
 #print(database.stock_data.shape)
 #print(database.stock_data[:10,-10:])
 model = RecurrentAnalyzer(50).to(device)
-optimizer = Adam(params=model.parameters(),lr=0.001)
+optimizer = Adam(params=model.parameters(),lr=0.0005)
 loss_fn = nn.MSELoss()
 
-EPOCH = 100
+minLoss = 1e9
+EPOCH = 5000
 batch_size = 64
 val_split = 0.2
 length = 100
 train_data = torch.tensor(database.to_data(length))
 #print(train_data[:10,:10])
-train_len = int(val_split*train_data.shape[1])
-val_data = train_data[-train_len:]
-train_data = train_data[:train_len]
-train_data = train_data.to(device)
-val_data = val_data.to(device)
+train_len = int((1-val_split)*train_data.shape[1])
+print(train_len)
+val_data = train_data[:,train_len:]
+train_data = train_data[:,:train_len]
+train_data = train_data.to(device)[...,None]
+val_data = val_data.to(device)[...,None]
 #print(train_data.dtype)
 for epoch in range(EPOCH):
     lossSum = 0
     for batch_i in range(train_len//batch_size):
         model.init_hidden()
-        batch_data = train_data[:, batch_i*batch_size : (batch_i+1)*batch_size][...,None]
+        batch_data = train_data[:, batch_i*batch_size : (batch_i+1)*batch_size]
         #print(batch_data[:-1,...].shape)
         preds = model(batch_data[:-1,...])
         #print(preds)
@@ -45,4 +48,16 @@ for epoch in range(EPOCH):
     lossSum/=(train_len//batch_size)
     print(f'training epoch {epoch}/{EPOCH}; loss: {lossSum}')
     writer.add_scalar('Loss/train', lossSum, epoch)
+    if epoch % 20 == 0:
+        model.eval()
+        with torch.no_grad():
+            #print(val_data.shape)
+            model.init_hidden()
+            preds = model(val_data[:-1,...])
+            loss = loss_fn(preds, val_data[1:])
+            if loss < minLoss:
+                minLoss = loss
+                torch.save(model.state_dict(), 'rnn_baseline')
+            print(f'evaluating epoch {epoch}/{EPOCH}; loss: {loss}')
+            writer.add_scalar('Loss/val', loss, epoch)
 
